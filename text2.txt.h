0b6d8686c570e0aaffc8836b65ecab19f52fc7e2
lanjing99 <84336951@qq.com>
2016-10-14 17:27:33 +0800

    for YYModel

---
+++ text2.txt.h
@@ -0,0 +1,18 @@
+
+
+@interface model_data : NSObject
+
+@property (nonatomic, strong, nullable)  NSString   * FuiVoiceCreated;
+
+@property (nonatomic, strong, nullable)  NSString   * FuiCodeUpdated;
+
+@property (nonatomic, strong, nullable)  NSString   * FuiTryTime;
+
+@property (nonatomic, strong, nullable)  NSString   * FuiCreated;
+
+@property (nonatomic, strong, nullable)  NSString   * FsCode;
+
+@property (nonatomic, strong, nullable)  NSString   * FsMobile;
+
+@end 
+
