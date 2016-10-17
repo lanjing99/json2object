f30b1335430e27bb0826d2ebad760490be748906
lanjing99 <84336951@qq.com>
2016-10-17 14:28:05 +0800

    fix bug, add JSON to YYModel object

--- JSON2YYModelObject.py
+++ JSON2YYModelObject.py
@@ -12,11 +12,11 @@ class jsonObject:
     
     file_name = ''
     
-    new_line      = "\n\n"
+    new_line      = "\n"
     
     protocol     = "@protocol "
     
-    end          = "@end "
+    end          = "@end \n"
     
     interface    = "@interface "
     
@@ -26,25 +26,25 @@ class jsonObject:
     
     primitive_prefix = "@property (nonatomic, assign) "
      
-    number_prefix =  " NSNumber  "
+    number_prefix =  "NSNumber "
     
     number = " NSNumber "
     
-    integer_prefix =  " NSInteger  "
+    integer_prefix =  "NSInteger  "
     
-    bool_prefix =  " BOOL  "
+    bool_prefix =  "BOOL "
     
-    float_prefix =  " float  "
+    float_prefix =  "float "
     
-    long_prefix =  " long  "
+    long_prefix =  "long "
     
-    string_prefix = " NSString  "
+    string_prefix = "NSString "
 
     optional_prefix = " "
     
     string = " NSString "
     
-    pointer = " * "
+    pointer = "*"
     
     class_seperator = "_"
     
@@ -100,12 +100,12 @@ class jsonObject:
         
     
 
-    def parseJson (self, decoded_json_string, class_name,isprotocol ):
+    def parseJson (self, decoded_json_string, class_name, isprotocol):
         #        if isprotocol == True :
         #    self.class_define_array.append(self.protocol+ class_name +" "+self.new_line+self.end+self.new_line)
         self.class_array.append(class_name)
         class_define_string =  self.new_line + self.interface +  class_name + self.inherit_operator + self.base_class
-        class_custome_string = ""
+        class_modelCustomPropertyMapper_string = ""
         top_class_name = class_name
         for  k in decoded_json_string.keys():
             class_name = top_class_name
@@ -113,10 +113,10 @@ class jsonObject:
             if isinstance(v,float) or isinstance(v,int) or isinstance(v,long) or isinstance(v,bool):
                 class_define_string += self.new_line + self.object_prefix + self.number_prefix + self.pointer+ k + self.line_end
             elif isinstance(v,basestring) :
-                class_define_string += self.new_line + self.object_prefix +  self.string_prefix + self.pointer + k+ self.line_end
+                class_define_string += self.new_line + self.object_prefix + self.string_prefix + self.pointer + k+ self.line_end
             elif isinstance(v,dict):
                 class_name = class_name + self.class_seperator + k
-                class_define_string += self.new_line + self.object_prefix + class_name + self.optional_prefix + self.pointer + k + self.line_end
+                class_define_string += self.new_line + self.object_prefix + class_name + " " + self.pointer + k + self.line_end
                 self.parseJson(v,class_name,False)
             elif isinstance(v,list) or isinstance(v,tuple) :
                 if len(v) == 0 :
@@ -127,31 +127,33 @@ class jsonObject:
                     subValue = value[0]
                     class_name = class_name + self.class_seperator + '0'
                     class_define_string += self.new_line + self.object_prefix + "NSArray<NSArray <"+ class_name + ">*>*"  + k + self.line_end
-#                    class_custome_string += "@\"" + k + "\" : " + class_name + ".class," + self.new_line
+#                    class_modelCustomPropertyMapper_string += "@\"" + k + "\" : " + class_name + ".class," + self.new_line
                     self.parseJson(subValue,class_name,True)
                 elif isinstance(value,float) or isinstance(value,int) or isinstance(value,long) or isinstance(value,bool):
-                    class_define_string += self.new_line + self.object_prefix + " NSMutableArray <NSNumber *> * " + k + self.line_end
+                    class_define_string += self.new_line + self.object_prefix + "NSArray <NSNumber *> * " + k + self.line_end
                 elif isinstance(value,basestring):
-                    class_define_string += self.new_line + self.object_prefix + " NSMutableArray <NSString *> * " + k + self.line_end
+                    class_define_string += self.new_line + self.object_prefix + "NSArray <NSString *> * " + k + self.line_end
                 else:
                     class_name = class_name + self.class_seperator + k
-                    class_define_string += self.new_line + self.object_prefix + " NSMutableArray <" + class_name + "> * " + k + self.line_end
-                    class_custome_string += "@\"" + k + "\" : " + class_name + ".class," + self.new_line
+                    class_define_string += self.new_line + self.object_prefix + "NSArray <" + class_name + " " + self.pointer + "> " + self.pointer + k + self.line_end
+                    class_modelCustomPropertyMapper_string += "\t\t\t" + "@\"" + k + "\" : " + class_name + ".class," + self.new_line
                     self.parseJson(value,class_name,True)
         # 类头文件内容
-        class_define_string += self.new_line+self.end+self.new_line
+        class_define_string += self.new_line + self.end + self.new_line
         self.class_define_array.append(class_define_string)
 
         # 类实现实现文件内容
-        class_implemtion_head_string = self.new_line + self.implementation + class_name + self.new_line
-        class_implemtion_end_string  = self.new_line + self.end  + self.new_line
-        class_implementation_array.append(class_implemtion_head_string)
-        if len(class_custome_string):
-            class_implementation_array.append("+ (NSDictionary *)modelCustomPropertyMapper{ \nreturn @{")
-            class_implementation_array.append(class_custome_string)
-            class_implementation_array.append("};\n}")
-        class_implementation_array.append(class_implemtion_end_string)
+        class_implemtion_head_string = self.new_line + self.implementation + top_class_name + self.new_line
+        self.class_implementation_array.append(class_implemtion_head_string)
+
+        if len(class_modelCustomPropertyMapper_string) != 0:
+            self.class_implementation_array.append("+ (NSDictionary *)modelCustomPropertyMapper{ \n\treturn @{\n")
+            self.class_implementation_array.append(class_modelCustomPropertyMapper_string)
+            self.class_implementation_array.append("\t};\n}")
+
+        class_implemtion_end_string = self.new_line + self.end + self.new_line
+        self.class_implementation_array.append(class_implemtion_end_string)
 
 
-object = jsonObject("text2.txt","model")
+object = jsonObject("testing.txt","model")
 object.write2File();
